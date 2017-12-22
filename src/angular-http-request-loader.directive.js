c13c4b22f9bad897fc19566b0e1fe7c3d65e5470
Nikhil Rane <nikhilrane1992@gmail.com>
2017-12-22 19:05:30 +0530

    setup http request loader project

---
+++ src/angular-http-request-loader.directive.js
@@ -0,0 +1,19 @@
+
+angular.module("http.request.loader", [])
+.directive('httpRequestLoader', ['$http', function($http) {
+    return {
+      restrict: 'E',
+      template: '<div class="loading-icon"></div>',
+      link: function(scope, element, attrs) {
+        scope.isLoading = function() {
+          return $http.pendingRequests.length > 0;
+        };
+        scope.$watch(scope.isLoading, function(value) {
+          if (value)
+            element.removeClass('ng-hide');
+          else
+            element.addClass('ng-hide');
+        });
+      }
+    }
+}]);
---
+++ src/angular-page-loader.module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('App', [
+    'http.request.loader'
+]);
+
+.controller('MainCtrl', function($scope) {
+    console.log("MainCtrl Loads")
+});
