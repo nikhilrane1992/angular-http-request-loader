4284b3efba5f5228c0cdde73f78472e10c25726d
Nikhil Rane <nikhilrane1992@gmail.com>
2017-12-26 12:03:18 +0530

    Intermidiate commit

--- dist/angular-http-request-loader.js
+++ dist/angular-http-request-loader.js
@@ -1,6 +1,5 @@
 /* angular-http-request-loader - v1.0.2 by Nikhil Rane - 2017-12-22 */
 
-
 angular.module("angular.http.request.loader", [])
 .directive('httpRequestLoader', ['$http', function($http) {
     return {
